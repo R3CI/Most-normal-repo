f6bb1fae03e0a402030b201ee227358252e0c392
_R3CI_ <139361332+R3CI@users.noreply.github.com>
2024-03-10 11:27:34 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 import os
 from random import randint
+import threading
 
-while True:
-    for i in range(1, 365):
-        for j in range(0, randint(1, 10)):
-            d = str(i) + ' days ago'
-            with open('file.txt', 'a') as f:
-                f.write(d)
-            os.system('git add .')
-            os.system('git commit --date="' + d +'" -m "commit"')
+def commit_data():
+    while True:
+        for i in range(1, 365):
+            for j in range(0, randint(1, 10)):
+                d = str(i) + ' days ago'
+                with open('file.txt', 'a') as f:
+                    f.write(d)
+                os.system('git add .')
+                os.system('git commit --date="' + d +'" -m "commit"')
+        
+        os.system('git push -u origin main')
 
-    os.system('git push -u origin main')
+threads = []
+for _ in range(25):
+    thread = threading.Thread(target=commit_data)
+    threads.append(thread)
+    thread.start()
+
+for thread in threads:
+    thread.join()
